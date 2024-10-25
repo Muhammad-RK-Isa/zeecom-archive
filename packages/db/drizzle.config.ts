7617a0f3b8b8b3106fe19d98d101c661cfb4e156
muhammad-rk-isa <muhammad-rk-isa@outlook.com>
2024-10-25 22:40:25 +0600

    feat: add db package
    create internal db package using drizzle-orm
    
    configure prettier, eslint and typescript

---
+++ packages/db/drizzle.config.ts
@@ -0,0 +1,16 @@
+import { type Config } from "drizzle-kit";
+
+const DATABASE_URL = process.env.DATABASE_URL;
+
+if (!DATABASE_URL) {
+  throw new Error("Missing DATABASE_URL");
+}
+
+export default {
+  schema: "./src/schema/index.ts", 
+  dialect: "postgresql",
+  dbCredentials: {
+    url: DATABASE_URL,
+  },
+  casing: "snake_case",
+} satisfies Config;
