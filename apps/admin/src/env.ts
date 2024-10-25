318dd891dde3e9f73fa6a19fb3cfbc11623c619a
muhammad-rk-isa <muhammad-rk-isa@outlook.com>
2024-10-26 00:37:40 +0600

    chore: clean unnecessary comments

--- apps/admin/src/env.ts
+++ apps/admin/src/env.ts
@@ -3,10 +3,6 @@ import { vercel } from "@t3-oss/env-nextjs/presets";
 import { z } from "zod";
 
 export const env = createEnv( {
-  /**
-   * Specify your server-side environment variables schema here. This way you can ensure the app
-   * isn't built with invalid env vars.
-   */
   extends: [ vercel() ],
   shared: {
     NODE_ENV: z
@@ -16,31 +12,12 @@ export const env = createEnv( {
   server: {
     DATABASE_URL: z.string().url(),
   },
-  /**
-   * Specify your client-side environment variables schema here. This way you can ensure the app
-   * isn't built with invalid env vars. To expose them to the client, prefix them with
-   * `NEXT_PUBLIC_`.
-   */
   client: {
-    // NEXT_PUBLIC_CLIENTVAR: z.string(),
   },
   experimental__runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
   },
-  /**
-   * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
-   * middlewares) or client-side so we need to destruct manually.
-   */
-  /**
-   *
-   * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
-   * useful for Docker builds.
-   */
   skipValidation: 
      process.env.npm_lifecycle_event === "lint",
-  /**
-   * Makes it so that empty strings are treated as undefined. `SOME_VAR: z.string()` and
-   * `SOME_VAR=''` will throw an error.
-   */
   emptyStringAsUndefined: true,
 } );
