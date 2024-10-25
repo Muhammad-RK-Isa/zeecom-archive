6a36892efd74035a1e5e878efcec8685dbd74bb2
muhammad-rk-isa <muhammad-rk-isa@outlook.com>
2024-10-26 01:08:33 +0600

    feat: create providers
    - add theme provider
    - add tRPC provider
    - add tanstack query devtools

---
+++ apps/admin/src/providers/index.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import React from 'react'
+import { TRPCReactProvider } from "~/trpc/react";
+import { ThemeProvider } from './theme-provider';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { env } from '~/env';
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <TRPCReactProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+      {env.NODE_ENV !== "production"
+        ? <ReactQueryDevtools buttonPosition='bottom-right' position='bottom' />
+        : null
+      }
+    </TRPCReactProvider>
+  )
+}
+
+export default Providers
