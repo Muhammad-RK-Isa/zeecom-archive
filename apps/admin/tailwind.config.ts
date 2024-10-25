d07c8dd8d93d50abafa2d5af4e3efc89f6820f71
muhammad-rk-isa <muhammad-rk-isa@outlook.com>
2024-10-26 01:00:29 +0600

    feat(shadcn): add required dependencies for shadcn ui
    - add lucide-react
    - add @radix-ui/react-icons

--- apps/admin/tailwind.config.ts
+++ apps/admin/tailwind.config.ts
@@ -3,10 +3,11 @@ import { type Config } from "tailwindcss";
 import base from "@zeecom/tailwind-config/web"
 
 export default {
-  content: [...base.content],
-  presets: [base],
-  theme: {
-    extend: {
-    },
-  },
+	darkMode: ["class"],
+	content: [...base.content],
+	presets: [base],
+	theme: {
+		extend: {
+		},
+	},
 } satisfies Config;
